119212c3cb9548426b74940ff857d92a086f663c
Aditya Chakraborty <aditya.chakraborty@promignis.com>
2022-10-12 00:15:28 +0530

    Create ActivityCard component and display in grid

---
+++ src/components/ActivityCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ActivityCard.css'
+
+const ActivityCard = (props) => {
+  return (
+    <div className="card">
+        Abb jabba dabba
+    </div>
+  )
+}
+
+export default ActivityCard
