3bd32f8bf2bab5d2e22fec2771d6678557a096b8
Aditya Chakraborty <aditya.chakraborty@promignis.com>
2022-10-25 17:56:46 +0530

    Add title and ellipsis to Activity card

--- src/App.js
+++ src/App.js
@@ -27,37 +27,48 @@ function App() {
         </section>
         {[{
           id: 1,
+          title: 'Work',
           bgColor: 'hsl(15, 100%, 70%)',
           icon: workIcon,
         },
         {
           id: 2,
+          title: 'Play',
           bgColor: 'hsl(195, 74%, 62%)',
           icon: playIcon,
         },
         {
           id: 3,
+          title: 'Study',
           bgColor: 'hsl(348, 100%, 68%)',
           icon: studyIcon,
         },
         {
           id: 4,
+          title: 'Exercise',
           bgColor: 'hsl(145, 58%, 55%)',
           icon: exerciseIcon,
         },
         {
           id: 5,
+          title: 'Social',
           bgColor: 'hsl(264, 64%, 52%)',
           icon: socialIcon,
         },
         {
           id: 6,
+          title: 'Self Care',
           bgColor: 'hsl(43, 84%, 65%)',
           icon: selfCareIcon,
         },
     ].map(val => {
           return (
-            <ActivityCard key={val.id} bgColor={val.bgColor} img={val.icon} />
+            <ActivityCard
+              key={val.id}
+              bgColor={val.bgColor}
+              img={val.icon}
+              title={val.title}
+            />
           )
         })}
       </div>
--- src/components/ActivityCard.js
+++ src/components/ActivityCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './ActivityCard.css'
+import ellipsis from '../images/icon-ellipsis.svg'
 
 const ActivityCard = (props) => {
   return (
@@ -7,7 +8,12 @@ const ActivityCard = (props) => {
       className="card" 
       style={{backgroundColor: props.bgColor, backgroundImage: `url(${props.img})`}}
     >
-      <div className="activity-container" />
+      <div className="activity-container">
+        <div className="header">
+          <header>{props.title}</header>
+          <img src={ellipsis} alt='ellipsis' />
+        </div>
+      </div>
     </div>
   )
 }
